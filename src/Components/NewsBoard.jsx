2f80e13da468b4b4a3db26b5cf4fb4fcc4a7c295
Lwin <lwindev97@gmail.com>
2024-04-27 13:16:18 +0630

    First commit and project completed

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import NewsItem from "./NewsItem";
+
+NewsBoard.propTypes = {
+  category: PropTypes.string,
+};
+
+function NewsBoard({ category }) {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
+  }, [category]);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default NewsBoard;
